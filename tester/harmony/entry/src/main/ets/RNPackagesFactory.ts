934913d92f3923a4e3995d5e8ee4e2cffabe6421
Zofia Glapa <zofia.glapa@swmansion.com>
2024-02-07 12:33:56 +0100

    Integrate the reanimated example app into tester

--- tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
+++ tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
@@ -1,6 +1,8 @@
 import type {RNPackageContext, RNPackage} from 'rnoh/ts';
 import {ReanimatedPackage} from 'rnoh-reanimated/ts';
+import {GestureHandlerPackage } from "rnoh-gesture-handler/ts";
+
 
 export function createRNPackages(ctx: RNPackageContext): RNPackage[] {
-  return [new ReanimatedPackage(ctx)];
+  return [new ReanimatedPackage(ctx), new GestureHandlerPackage(ctx)];
 }
