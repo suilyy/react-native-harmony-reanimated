cdcdccac2eb339240d4059bb612317a406305a0a
wangyue <w60058851@china.huawei.com>
2024-10-09 19:46:26 +0800

    ..

--- react-native-harmony-reanimated/scripts/deploy.js
+++ react-native-harmony-reanimated/scripts/deploy.js
@@ -53,73 +53,73 @@ function runDeployment() {
         stdio: 'inherit',
       });
 
-      // rl.question(
-      //   `Please generate ${HAR_FILE_OUTPUT_PATH} file. Open DevEco Studio, select any file in '${MODULE_NAME}' module, and run Build > Make Module '${MODULE_NAME}'.\nOnce you finish type 'done': `,
-      //   (answer) => {
-      //     if (answer !== 'done') {
-      //       console.log('Deployment aborted');
-      //       process.exit(1);
-      //     }
-      //     console.log(
-      //       `Copying ${`../${HAR_FILE_OUTPUT_PATH}`} to ./harmony dir`
-      //     );
-      //     if (!fs.existsSync(`../${HAR_FILE_OUTPUT_PATH}`)) {
-      //       console.log(`Couldn't find ${HAR_FILE_OUTPUT_PATH}.`);
-      //       process.exit(1);
-      //     }
-      //     fs.rmSync('./harmony', { recursive: true, force: true });
-      //     fs.mkdirSync('./harmony');
-      //     fs.renameSync(
-      //       `../${HAR_FILE_OUTPUT_PATH}`,
-      //       `./harmony/${MODULE_NAME}.har`
-      //     );
+      rl.question(
+        `Please generate ${HAR_FILE_OUTPUT_PATH} file. Open DevEco Studio, select any file in '${MODULE_NAME}' module, and run Build > Make Module '${MODULE_NAME}'.\nOnce you finish type 'done': `,
+        (answer) => {
+          if (answer !== 'done') {
+            console.log('Deployment aborted');
+            process.exit(1);
+          }
+          console.log(
+            `Copying ${`../${HAR_FILE_OUTPUT_PATH}`} to ./harmony dir`
+          );
+          if (!fs.existsSync(`../${HAR_FILE_OUTPUT_PATH}`)) {
+            console.log(`Couldn't find ${HAR_FILE_OUTPUT_PATH}.`);
+            process.exit(1);
+          }
+          fs.rmSync('./harmony', { recursive: true, force: true });
+          fs.mkdirSync('./harmony');
+          fs.renameSync(
+            `../${HAR_FILE_OUTPUT_PATH}`,
+            `./harmony/${MODULE_NAME}.har`
+          );
 
-      //     // const changelogForCurrentVersion = execSync(
-      //     //   `npm run -s gen:changelog`
-      //     // ).toString();
-      //     // updateChangelog(version, changelogForCurrentVersion);
+          // const changelogForCurrentVersion = execSync(
+          //   `npm run -s gen:changelog`
+          // ).toString();
+          // updateChangelog(version, changelogForCurrentVersion);
 
-      //     execSync(`npm publish --dry-run`, { stdio: 'inherit' });
+          execSync(`npm publish --dry-run`, { stdio: 'inherit' });
 
-      //     // rl.question(
-      //     //   'Are changes good to be published and pushed to the upstream? (yes/no): ',
-      //     //   async (answer) => {
-      //     //     if (answer.toLowerCase() === 'yes') {
-      //     //       execSync(`npm publish`, { stdio: 'inherit' });
-      //     //       console.log('NPM Package was published successfully.');
-      //     //       execSync(
-      //     //         `git checkout -b release-${UNSCOPED_NPM_PACKAGE_NAME}-${version}`
-      //     //       );
-      //     //       execSync('git add -A');
-      //     //       execSync(
-      //     //         `git commit -m "release: ${UNSCOPED_NPM_PACKAGE_NAME}@${version}"`,
-      //     //         {
-      //     //           stdio: 'inherit',
-      //     //         }
-      //     //       );
-      //     //       execSync(`git push -u origin HEAD --no-verify`, {
-      //     //         stdio: 'inherit',
-      //     //       });
+          rl.question(
+            'Are changes good to be published and pushed to the upstream? (yes/no): ',
+            async (answer) => {
+              if (answer.toLowerCase() === 'yes') {
+                execSync(`npm publish`, { stdio: 'inherit' });
+                console.log('NPM Package was published successfully.');
+                execSync(
+                  `git checkout -b release-${UNSCOPED_NPM_PACKAGE_NAME}-${version}`
+                );
+                execSync('git add -A');
+                execSync(
+                  `git commit -m "release: ${UNSCOPED_NPM_PACKAGE_NAME}@${version}"`,
+                  {
+                    stdio: 'inherit',
+                  }
+                );
+                execSync(`git push -u origin HEAD --no-verify`, {
+                  stdio: 'inherit',
+                });
 
-      //     //       execSync(`git tag v${version}`);
-      //     //       execSync(`git push -u origin v${version} --no-verify`, {
-      //     //         stdio: 'inherit',
-      //     //       });
-      //     //       const mergeRequestId = await createMergeRequest(
-      //     //         `release-${UNSCOPED_NPM_PACKAGE_NAME}-${version}`,
-      //     //         `release: ${UNSCOPED_NPM_PACKAGE_NAME}@${version}`
-      //     //       );
-      //     //       console.log(`Please merge the following Merge Request:\n
-      //     //       https://gl.swmansion.com/rnoh/${UNSCOPED_NPM_PACKAGE_NAME}/-/merge_requests/${mergeRequestId}`);
-      //     //       rl.close();
-      //     //     } else {
-      //     //       console.log('Deployment aborted.');
-      //     //       rl.close();
-      //     //     }
-      //     //   }
-      //     // );
-      //   }
-      // );
+                execSync(`git tag v${version}`);
+                execSync(`git push -u origin v${version} --no-verify`, {
+                  stdio: 'inherit',
+                });
+                const mergeRequestId = await createMergeRequest(
+                  `release-${UNSCOPED_NPM_PACKAGE_NAME}-${version}`,
+                  `release: ${UNSCOPED_NPM_PACKAGE_NAME}@${version}`
+                );
+                console.log(`Please merge the following Merge Request:\n
+                https://gl.swmansion.com/rnoh/${UNSCOPED_NPM_PACKAGE_NAME}/-/merge_requests/${mergeRequestId}`);
+                rl.close();
+              } else {
+                console.log('Deployment aborted.');
+                rl.close();
+              }
+            }
+          );
+        }
+      );
     }
   );
 }
